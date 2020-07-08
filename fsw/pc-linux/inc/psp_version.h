d7f1b49019179a05d4eb6f200a022e09a833d168
Gerardo E. Cruz-Ortiz <gerardo.e.cruz-ortiz@nasa.gov>
2020-07-08 12:23:15 -0400

    Resolve #177, Add baseline and build to version.h
    Add new macros for build baseline and number to psp_version.h files
    Add new element to PSP configuration structure.
    Add new macro CFE_PSP_VERSION in cfe_psp.h

--- fsw/pc-linux/inc/psp_version.h
+++ fsw/pc-linux/inc/psp_version.h
@@ -19,23 +19,56 @@
 */
 
 /*
-**  File: psp_version.h
-**
-**  Purpose:
-**  Provide version identifiers for the cFE Platform Support Packages (PSP).
-**
-*/
+ *  File: psp_version.h
+ *
+ *  Purpose:
+ *  Provide version identifiers for the cFE Platform Support Packages (PSP).
+ *  See cfe documentation for version and build number descriptions
+ *
+ */
 
 #ifndef _psp_version_
 #define _psp_version_
 
+/* Development Build Macro Definitions */
+#define CFE_PSP_IMPL_BUILD_NUMBER 68
+#define CFE_PSP_IMPL_BUILD_BASELINE "v1.4.0+dev"
 
 /*
-** Macro Definitions
-*/
-#define CFE_PSP_IMPL_MAJOR_VERSION          1
-#define CFE_PSP_IMPL_MINOR_VERSION          4
-#define CFE_PSP_IMPL_REVISION               14
-#define CFE_PSP_IMPL_MISSION_REV            0
+ * Macro Definitions
+ * ONLY APPLY for OFFICIAL releases
+ */
+#define CFE_PSP_IMPL_MAJOR_VERSION 1
+#define CFE_PSP_IMPL_MINOR_VERSION 4
+#define CFE_PSP_IMPL_REVISION      0
+#define CFE_PSP_IMPL_MISSION_REV   0
+
+#define CFE_PSP_IMPL_STR_HELPER(x) #x
+#define CFE_PSP_IMPL_STR(x)        CFE_PSP_IMPL_STR_HELPER(x)
+
+/* Baseling git tag + Number of commits since baseline */
+#define CFE_PSP_IMPL_VERSION CFE_PSP_IMPL_BUILD_BASELINE CFE_PSP_IMPL_STR(CFE_PSP_IMPL_BUILD_NUMBER)
+
+#define CFE_PSP_IMPL_VERSION_STRING                                                                      \
+    " PSP Development Build\n " CFE_PSP_IMPL_VERSION " (Codename: Bootes)" /* Codename for current development */ \
+    "\n Last Official Release: psp v1.4.0"                        /* For full support please use this version */
+
+/* Use the following templates for Official Releases ONLY */
+
+/* Official Release format for CFE_PSP_IMPLVERSION */
+  /*
+  #define CFE_PSP_IMPL_VERSION "v"            \
+       CFE_PSP_IMPL_STR(CFE_PSP_IMPL_MAJOR_VERSION) "." \
+       CFE_PSP_IMPL_STR(CFE_PSP_IMPL_MINOR_VERSION) "." \
+       CFE_PSP_IMPL_STR(CFE_PSP_IMPL_REVISION) "."      \
+       CFE_PSP_IMPL_STR(CFE_PSP_IMPL_MISSION_REV)
+  */
+
+  /* Official Release CFE_PSP_IMPLVERSION_STRING Format */
+  /*
+  #define CFE_PSP_IMPL_VERSION_STRING " PSP " CFE_PSP_IMPL_VERSION
+  */
+  
+/* END TEMPLATES */
 
 #endif  /* _psp_version_ */
