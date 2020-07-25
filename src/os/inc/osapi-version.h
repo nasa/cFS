d52023bd636958e1fd53a6aa234301a83b59bbcd
Gerardo E. Cruz-Ortiz <gerardo.e.cruz-ortiz@nasa.gov>
2020-07-25 16:05:00 -0400

    HOTFIX IC-20200624, Remove commented code

--- src/os/inc/osapi-version.h
+++ src/os/inc/osapi-version.h
@@ -18,65 +18,52 @@
  *  limitations under the License.
  */
 
-/*
- * File: osapi-version.h
- *
- * Purpose:
- *  The OSAL version numbers
- *  See cfe documentation for version and build number descriptions
- */
-
+ /*! @file osapi-version.h
+  * @brief Purpose:
+  *  @details Provide version identifiers for cFS' Operating System Abstraction Layer
+  *  See @ref cfsversions for version and build number and description
+  * 
+  */
 #ifndef _osapi_version_h_
 #define _osapi_version_h_
 
-/* Development Build Macro Definitions */
+/*
+ * Development Build Macro Definitions 
+ */
 #define OS_BUILD_NUMBER 242 
 #define OS_BUILD_BASELINE "v5.0.0+dev"
 
-/* Version Macro Definitions
-* ONLY APPLY for OFFICIAL release builds
-*/
-#define OS_MAJOR_VERSION 5 /**< @brief Major version number */
-#define OS_MINOR_VERSION 0 /**< @brief Minor version number */
-#define OS_REVISION      0 /**< @brief Revision number */
-#define OS_MISSION_REV   0 /**< @brief Mission revision */
+/*
+ * Version Macro Definitions
+ */
+#define OS_MAJOR_VERSION 5 /*!< @brief ONLY APPLY for OFFICIAL releases. Major version number. */
+#define OS_MINOR_VERSION 0 /*!< @brief ONLY APPLY for OFFICIAL releases. Minor version number. */
+#define OS_REVISION      0 /*!< @brief ONLY APPLY for OFFICIAL releases. Revision number. */
+#define OS_MISSION_REV   0 /*!< @brief ONLY USED by MISSION Implementations. Mission revision */
 
-/* Helper functions to concatenate integer macro definitions into a string */
-#define OS_STR_HELPER(x) #x
-#define OS_STR(x)        OS_STR_HELPER(x)
+/*
+ * Tools to construct version string
+ */ 
+#define OS_STR_HELPER(x) #x /*!< @brief Helper function to concatenate strings from integer */
+#define OS_STR(x)        OS_STR_HELPER(x) /*!< @brief Helper function to concatenate strings from integer */
 
-/* Development Build format for OS_VERSION */
-/* Baseline git tag + Number of commits since baseline */
-#define OS_VERSION OS_BUILD_BASELINE OS_STR(OS_BUILD_NUMBER)
+/*! @brief Development Build Version Number. 
+ * @details Baseline git tag + Number of commits since baseline. @n
+ * See @ref cfsversions for format differences between development and release versions.
+ */
+#define OS_VERSION OS_BUILD_BASELINE OS_STR(OS_BUILD_NUMBER) 
 
-/* Development Build format for OS_VERSION_STRING */
+/*! @brief Development Build Version String.
+ * @details Reports the current development build's baseline, number, and name. Also includes a note about the latest official version. @n
+ * See @ref cfsversions for format differences between development and release versions. 
+*/     
 #define OS_VERSION_STRING                                                          \
     " OSAL Development Build\n"                                                    \
     " " OS_VERSION " (Codename: Bootes)\n"  /* Codename for current development */ \
     " Latest Official Version: osal v5.0.0" /* For full support please use official release version */
 
-/* Use the following templates for Official Releases ONLY */
-
-/* Official Release format for OS_VERSION */
-  /*
-  #define OS_VERSION "v"            \
-       OS_STR(OS_MAJOR_VERSION) "." \
-       OS_STR(OS_MINOR_VERSION) "." \
-       OS_STR(OS_REVISION) "."      \
-       OS_STR(OS_MISSION_REV)
-  */
-
-  /* Official Release OS_VERSION_STRING Format */
-  /*
-  #define OS_VERSION_STRING " OSAL " OS_VERSION
-  */
-  
-/* END TEMPLATES */
-
-
-/*
- * Combine the revision components into a single value that application code can
-check against
+/*! @brief Combines the revision components into a single value 
+ * @details Applications can check against this number @n
  * e.g. "#if OSAL_API_VERSION >= 40100" would check if some feature added in
 OSAL 4.1 is present.
  */
