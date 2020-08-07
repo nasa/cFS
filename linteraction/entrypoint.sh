916a3588516d2e299d394e1770f75a1dba929d45
Stephen Scott <stephen99scott@gmail.com>
2020-08-07 15:51:17 -0400

    Removed build checks and ds/test_and_ground from cpplint check

--- linteraction/entrypoint.sh
+++ linteraction/entrypoint.sh
@@ -5,10 +5,10 @@ for folder in  "${folders[@]}"
 do
     echo ${folder}
     if [[ -d ${folder} ]]; then
-	command="cpplint --filter=-whitespace,-readability,-legal --recursive --exclude=apps/libcsp --exclude=apps/sample_app --exclude=apps/ci_lab --exclude=apps/io_lib --exclude=apps/sample_lib --exclude=apps/sch_lab --exclude=apps/to_lab --exclude=apps/*/fsw/unit_test ${folder}"
+	command="cpplint --filter=-whitespace,-readability,-legal,-build --recursive --exclude=apps/libcsp --exclude=apps/sample_app --exclude=apps/ci_lab --exclude=apps/io_lib --exclude=apps/sample_lib --exclude=apps/sch_lab --exclude=apps/to_lab --exclude=apps/ds/test_and_ground --exclude=apps/*/fsw/unit_test ${folder}"
 	echo ${command}
 	if ${command} | grep "error"; then
-	    echo "::error ::Review code formatting"
+	    echo "::error ::Review code!"
 	    exit 1
 	fi
     fi
