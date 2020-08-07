8d2cb19e5bc4afeac1ae4d99028107e71c6255c0
Stephen Scott <stephen99scott@gmail.com>
2020-08-06 22:14:54 -0400

    Added linter test workflow

---
+++ linteraction/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+folders={"apps"}
+for folder in  "${folders[@]}"
+do
+    echo ${folder}
+    if [[ -d ${folder} ]]; then
+	command="cpplint --recursive ${folder}/*"
+	echo ${command}
+	if ${command} | grep "error"; then
+	    echo "::error ::Review code formatting"
+	    exit 1
+	fi
+    fi
+done
