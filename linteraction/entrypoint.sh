27c34f38dbfe0c1596984253961445409ae76bfb
Stephen Scott <stephen99scott@gmail.com>
2020-08-07 15:40:24 -0400

    Exclude non-flight apps from cpplint check

--- linteraction/entrypoint.sh
+++ linteraction/entrypoint.sh
@@ -5,7 +5,7 @@ for folder in  "${folders[@]}"
 do
     echo ${folder}
     if [[ -d ${folder} ]]; then
-	command="cpplint --filter=-whitespace,-readability,-legal --recursive --exclude=apps/libcsp --exclude=apps/*/fsw/unit_test ${folder}"
+	command="cpplint --filter=-whitespace,-readability,-legal --recursive --exclude=apps/libcsp --exclude=apps/sample_app --exclude=apps/ci_lab --exclude=apps/io_lib --exclude=apps/sample_lib --exclude=apps/sch_lab --exclude=apps/to_lab --exclude=apps/*/fsw/unit_test ${folder}"
 	echo ${command}
 	if ${command} | grep "error"; then
 	    echo "::error ::Review code formatting"
