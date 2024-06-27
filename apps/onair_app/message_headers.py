7be1a844f6dc348d9479aae0aac5c4d47d4231ec
williamzhang0306 <williamzhang0306@gmail.com>
2024-06-27 13:45:28 -0400

    sbn talking to onair

---
+++ apps/onair_app/message_headers.py
@@ -0,0 +1,18 @@
+from ctypes import *
+import sbn_python_client as sbn
+
+
+# these classes were generated with ChatGPT
+class SAMPLE_APP_HkTlm_Payload_t(Structure):
+    _fields_ = [
+        ("CommandErrorCounter", c_uint8),
+        ("CommandCounter", c_uint8)
+        #("spare", c_uint8 * 2)
+    ]
+
+class SAMPLE_APP_HkTlm_t(Structure):
+    _pack_ = 1
+    _fields_ = [
+        ("TlmHeader", sbn.CFE_SB_Msg_t),
+        ("Payload", SAMPLE_APP_HkTlm_Payload_t)
+    ]
