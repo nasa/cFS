281386df9a6a5999161e4a207e1d1bb7eb32fb8b
williamzhang0306 <williamzhang0306@gmail.com>
2024-07-10 16:45:49 -0400

    update message_headers

--- apps/onair_app/message_headers.py
+++ apps/onair_app/message_headers.py
@@ -4,10 +4,11 @@ import sbn_python_client as sbn
 
 # these classes were generated with ChatGPT
 class SAMPLE_APP_HkTlm_Payload_t(Structure):
+    _pack_ = 1
     _fields_ = [
         ("CommandErrorCounter", c_uint8),
-        ("CommandCounter", c_uint8)
-        #("spare", c_uint8 * 2)
+        ("CommandCounter", c_uint8),
+        ("spare", c_uint8 * 2)       
     ]
 
 class SAMPLE_APP_HkTlm_t(Structure):
